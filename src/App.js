e39f1de103a652ec527054d0186fcf22401a42f9
mgusinov <mgusinov11@gmail.com>
2019-07-09 07:57:27 -0400

    Added functionality for having multiple notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Note from './Note/Note';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  constructor(props){
+    super(props);
+
+    //Going to setup the React state of our component
+    this.state = {
+      notes: [
+        { id: 1, noteContent: "Note 1 here!" },
+        { id: 2, noteContent: "Note 2 here!" },
+      ],
+    }
+  }
+
+  render() {
+    return (
+      <div className = "noteWrapper">
+        
+        <div className = "notesHeader">
+          <div className = "heading"> React & Firebase To-Do List </div>
+        </div>
+  
+        <div className = "notesBody">
+          {
+            this.state.notes.map((note) => {
+              return (
+                <Note noteContent={note.noteContent} noteID={note.id} key={note.id}/>
+              )
+            })
+          }
+  
+          
+        </div>
+  
+        <div className = "notesFooter">
+          Footer will go here...
+        </div>
+      
+      </div>
+    );
+  }
+  
 }
 
 export default App;
